109a23665b6b2b106c6969ff803345dc74d3a0d5
unknown <haroldorodsilva@gmail.com>
2020-10-14 13:58:21 -0400

    Ajuste busnag

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         });
     }
 
+    ErrorNotify.notify(err);
     console.error(err);
 
     return res.status(500).json({
